4d5aa37dccfd8a503115179f182201771c1f8bbc
MariannaGarbuzova <vesna09@tut.by>
2018-09-30 15:40:14 +0300

    solved

--- src/index.js
+++ src/index.js
@@ -1,5 +1,35 @@
 // PLEASE DON'T change function name
 module.exports = function makeExchange(currency) {
-    // Your code goes here!
+    let result = {};
+    if (currency <= 0) {
+        return result;
+    }
+
+    if (currency > 10000) {
+        return { error: "You are rich, my friend! We don't have so much coins for exchange" };
+    }
+
+    //H, Q, D, N P
+    const coins = [
+        { name: 'H', value: 50 },
+        { name: 'Q', value: 25 },
+        { name: 'D', value: 10 },
+        { name: 'N', value: 5 },
+        { name: 'P', value: 1 }
+    ];
+
+    let reminder = currency;
+    let coinsCount = 0;
+    for (let i = 0; i < coins.length; i++) {
+        var coin = coins[i];
+        coinsCount = Math.floor(reminder/coin.value);
+        reminder = reminder % coin.value;
+        if (coinsCount > 0){
+            result[coin.name] = coinsCount;
+        }
+    }
+
+    return result;
+
     // Return an object containing the minimum number of coins needed to make change
 }
